ade490dc9b782bce20f4d8ee78033b2fa7d22d0d
pepperpapa <zhongxin2506@outlook.com>
2016-09-08 19:06:01 +0800

    activeStyle and onlyActiveOnIndex of Link componet

--- app/App.js
+++ app/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Router, Route, Link, IndexRoute, hashHistory, browserHistory}
+import {Router, Route, Link, IndexLink, IndexRoute, hashHistory, browserHistory}
         from 'react-router';
 
 class App extends Component {
@@ -8,7 +8,11 @@ class App extends Component {
       <Router history={hashHistory}>
         <Route path="/" component={Container}>
           <IndexRoute component={Home} />
-  	      <Route path="/address" component={Address} />
+  	      <Route path="/address" component={Address}>
+            <IndexRoute component={TwitterFeed} />
+            <Route path="instagram" component={Instagram} />
+          </Route>
+          <Route path="/about" component={About} />
           <Route path="*" component={NotFound} />
         </Route>
       </Router>
@@ -17,7 +21,16 @@ class App extends Component {
 }
 
 const Home = () => <h1>hello from home!</h1>
-const Address = () => <h1>we are loacted at 555 Jackson St.</h1>
+const Instagram = () => <h3>Instagram Feed</h3>
+const TwitterFeed = () => <h3>Twitter Feed</h3>
+const Address = (props) => <div>
+  <br />
+  <Link to="/address">Twitter Feed</Link>&nbsp;
+  <Link to="/address/Instagram">Instagram Feed</Link>
+  <h1>we are loacted at 555 Jackson St.</h1>
+  {props.children}
+</div>
+const About = ()  => <h1>About Page</h1>
 const NotFound = () => <h1>404.. This page is not found!</h1>
 const Container = (props) => (
   <div>
@@ -27,9 +40,11 @@ const Container = (props) => (
 );
 const Nav = () => (
   <div>
-    <Link to="/">Home</Link>&nbsp;
-    <Link to="/address">Address</Link>
+    <Link onlyActiveOnIndex activeStyle={{color: '#53acff'}} to="/">Home</Link>&nbsp;
+    <Link activeStyle={{color: '#53acff'}} to="/address">Address</Link>&nbsp;
+    <Link activeStyle={{color: '#53acff'}} to="/about">About</Link>
   </div>
 );
+
 
 export default App;
